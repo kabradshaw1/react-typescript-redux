20517c3419a85cc76462c36fda53237d784cc7ae
kabradshaw1 <itllbok82@gmail.com>
2023-02-21 07:28:30 -0500

    comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+// Let's say we need to create a function
+// that should divide number `a` by number `b`
+// and return a round answer.
 export function divide(a: number, b:number): number {
+  // Sure, we cannot divide by 0,
+  // so in this case we will throw an error.
   if(b === 0) {
     throw new Error("You can't divide by zero.")
   }
-
+  // If everything is okay, we will return
+  // a round division result.
   return Math.round(a/b);
 }
 
