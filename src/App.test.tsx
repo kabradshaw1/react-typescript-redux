05937193bdc545bb347b99fe9bac2da8f23af00f
kabradshaw1 <itllbok82@gmail.com>
2023-02-21 07:42:50 -0500

    added comments

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import App, { divide } from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
@@ -20,7 +20,22 @@ test('renders learn react link', () => {
 describe('divide function', () => {
   describe('when given to integers', () => {
     it('should return a division result', () => {
-      
+      // Arrange: prepare function arguments
+      // and the expected division result.
+      // In this example 10 / 2 === 5:
+      const [a, b, expected] = [10, 2, 5];
+      // Here we use array destructuring 
+      // to assing `a === 10`, `b === 2`, 
+      // and `expected === 5`.
+
+      // Act: use the `divide` function 
+      // to get an actual function result
+      const result = divide(a, b);
+
+      // Assert: compare expected result
+      // with a function result.
+      expect(result).toEqual(expected);
+
     })
   })
 })
