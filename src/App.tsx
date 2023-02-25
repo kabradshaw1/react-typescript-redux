462e2f7ebe9285c9c452d06cc13bd649f5cfb50f
kabradshaw1 <itllbok82@gmail.com>
2023-02-25 14:07:11 -0500

    working on typescript tutorial and writing tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,81 +2,33 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Container from 'react-bootstrap/Container'
 
 // Let's say we need to create a function
 // that should divide number `a` by number `b`
 // and return a round answer.
-export function divide(a: number, b:number): number {
-  // Sure, we cannot divide by 0,
-  // so in this case we will throw an error.
-  if(b === 0) {
-    throw new Error("You can't divide by zero.")
-  }
-  // If everything is okay, we will return
-  // a round division result.
-  return Math.round(a/b);
-}
+// export function divide(a: number, b:number): number {
+//   // Sure, we cannot divide by 0,
+//   // so in this case we will throw an error.
+//   if(b === 0) {
+//     throw new Error("You can't divide by zero.")
+//   }
+//   // If everything is okay, we will return
+//   // a round division result.
+//   return Math.round(a/b);
+// }
 
-export function label(name: string) {
-  return `${name.toUpperCase()}`
-}
+// export function label(name: string) {
+//   return `${name.toUpperCase()}`
+// }
 
 function App() {
 
-  const [number, setNumber] = useState<number | string>(5);
-
-  const changeNumber = () => {
-    setNumber("10");
-  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container>
+      
+    </Container>
   );
 }
 
