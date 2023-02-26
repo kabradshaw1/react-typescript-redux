d94b52d1f7fd90f65d7876c377117191b6e9ba61
kabradshaw1 <itllbok82@gmail.com>
2023-02-26 09:36:50 -0500

    learning about react-hook-form

--- src/components/AddToList/index.tsx
+++ src/components/AddToList/index.tsx
@@ -1,9 +1,23 @@
 import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { constants } from 'http2';
 
 const AddToList = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  const onSubmit = (data: React.FormEvent<HTMLFormElement>) => console.log(data);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group>
+        <Button type='submit'>Submit</Button>
+      </Form.Group>
+    </Form>
+  )
+}
+
 
   // const [input, setInput] = useState({name:"", age: "", note: "", img: ""})
 
@@ -13,12 +27,3 @@ const AddToList = () => {
   //     [e.target.name]: e.target.value
   //   })
   // }
-  
-  return (
-    <Form onSubmit={handleChange}>
-      <Form.Group>
-        <Button type='submit'>Submit</Button>
-      </Form.Group>
-    </Form>
-  )
-}
