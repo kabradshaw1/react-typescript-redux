9acad04a6f6ad0be6bf068988728598f1857b5a8
kabradshaw1 <itllbok82@gmail.com>
2023-02-25 19:17:04 -0500

    learning about typescript tests

--- src/components/List/index.test.tsx
+++ src/components/List/index.test.tsx
@@ -1,9 +1,15 @@
-import List, {IProps} from './index';
+import List from './index';
 import { render } from '@testing-library/react';
 
 describe('component should exist', () => {
   it('should have some text',  () => {
-
+    let people = [
+      {
+        name: 'Sam',
+        age: 22,
+        url: 'http://www.webpage.com'
+      }
+    ]
     const { getByText } = render(<List people={people}/>);
   })
 })
