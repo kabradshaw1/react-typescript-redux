b87bc82fb1d5659d8760fd5c039fbae34bc0ca56
kabradshaw1 <itllbok82@gmail.com>
2023-03-03 17:29:13 -0500

    learning react-form-hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
 // import { useSelector, useDispatch } from 'react-redux';
-import {Controller, useForm} from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 // import { RootState } from './app/store';
-import { Form, Button, Row, Col} from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 interface FormData {
   firstName: string
 }
 
 function App() {
-  const { control, handleSubmit } = useForm<FormData>({
+  const { control, handleSubmit, formState:{errors} } = useForm<FormData>({
     defaultValues: {
       firstName: "",
     },
@@ -25,8 +25,23 @@ function App() {
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>First name</Form.Label>
-          <Controller name="firstName" control={control} render={({field}) => (<Form.Control type="text" {...field} placeholder="First name"/>)}/>
+          <Controller 
+            name="firstName" 
+            control={control} 
+            rules={{required:true}} 
+            render={({field}) => (
+              <Form.Control 
+                isInvalid={errors.firstName ? true: undefined} 
+                type="text" {...field} 
+                placeholder="First name"
+              />
+            )}
+          />
+          <Form.Control.Feedback type="invalid">
+            First name is required.
+          </Form.Control.Feedback>
         </Form.Group>
+
       </Row>
       <Button type="submit">Submit Form</Button>
     </Form>
