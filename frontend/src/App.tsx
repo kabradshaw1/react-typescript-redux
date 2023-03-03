de84ef77e6657919a262a9ff6a1e39e5bcdefba4
kabradshaw1 <itllbok82@gmail.com>
2023-03-03 17:38:36 -0500

    learning react-form-hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,14 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 
 interface FormData {
   firstName: string
+  age: number
 }
 
 function App() {
   const { control, handleSubmit, formState:{errors} } = useForm<FormData>({
     defaultValues: {
       firstName: "",
+      age:"",
     },
   });
 
@@ -23,7 +25,7 @@ function App() {
   return (
     <Form noValidate onSubmit={handleSubmit(formSubmit)}>
       <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
+        <Form.Group as={Col} md="4">
           <Form.Label>First name</Form.Label>
           <Controller 
             name="firstName" 
@@ -41,7 +43,15 @@ function App() {
             First name is required.
           </Form.Control.Feedback>
         </Form.Group>
+        <Form.Group as={Col} md="4">
+          <Form.Label>Age</Form.Label>
+          <Controller
+            name="age"
+            control={control}
+            rules={{max:30, min: 20}}
 
+          />
+        </Form.Group>
       </Row>
       <Button type="submit">Submit Form</Button>
     </Form>
