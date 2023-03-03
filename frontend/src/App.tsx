6dcd827450b7aaa900f75f028f88ccb6db2b8576
kabradshaw1 <itllbok82@gmail.com>
2023-03-03 13:43:22 -0500

    removed redux stuff, and trying react-hook-form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,107 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
-// import List from './components/List';
-// import AddToList from './components/AddToList';
 import { useSelector, useDispatch } from 'react-redux';
-import { addReservation } from './features/reservationSlice';
+import {useForm} from 'react-hook-form';
 import { RootState } from './app/store';
-import ReservationCard from './components/ReservationCard';
-import CustomerCard from './components/CustomerCard';
-
 import { Form, Button } from 'react-bootstrap';
 
-// import { useForm } from 'react-hook-form'; 
-
-
-interface IState {
-  people: {
-    name: string
-    age: number
-    url: string
-    note?: string
-  }[]
-}
-
-// type Reservation {
-
-// }
-
 function App() {
-  // This is entended to compare how easy it is to use the react-hook-form vs doing it with regular typescript
-  // const { register, handleSubmit, watch, formState: { errors } } = useForm()
-
-  // const form = useSelector((state: RootState) => state.form.value)
-  
-  // const [people, setPeople] = useState<IState['people']>([]);
-
-  // This is using the method that Laith Academy used
-  const dispatch = useDispatch();
-
-  const [reservationNameInput, setReservationNameInput] = useState("");
-
-  const reservations = useSelector((state: RootState) => state.reservations.value);
-
-  const customers = useSelector((state: RootState) => state.customer.value);
-
-  const handleAddReservations = () => {
-    if(!reservationNameInput) return;
-    dispatch(addReservation(reservationNameInput));
-    setReservationNameInput("")
-  };
+  const {register} = useForm()
 
   return (
-<div className="App">
-      <div className="container">
-        <div className="reservation-container">
-          <div>
-            <h5 className="reservation-header">Reservations</h5>
-            <div className="reservation-cards-container">
-              {reservations.map((name, index) => {
-                return <ReservationCard name={name} index={index} />;
-              })}
-            </div>
-          </div>
-          <div className="reservation-input-container">
-            <input
-              value={reservationNameInput}
-              onChange={(e) => {
-                setReservationNameInput(e.target.value);
-              }}
-            />
-            <button onClick={handleAddReservations}>Add</button>
-          </div>
-        </div>
-        <div className="customer-food-container">
-          {customers.map(customer => {
-            return <CustomerCard id={customer.id} name={customer.name} food={customer.food}/>
-          })}
-        </div>
-      </div>
+    <div>
+      <input {...register("userName")} placeholder="username" />
     </div>
   );
 }
 
 export default App;
 
-// Let's say we need to create a function
-// that should divide number `a` by number `b`
-// and return a round answer.
-// export function divide(a: number, b:number): number {
-//   // Sure, we cannot divide by 0,
-//   // so in this case we will throw an error.
-//   if(b === 0) {
-//     throw new Error("You can't divide by zero.")
-//   }
-//   // If everything is okay, we will return
-//   // a round division result.
-//   return Math.round(a/b);
-// }
-
-// export function label(name: string) {
-//   return `${name.toUpperCase()}`
-// }
-{/* <h1>People Invited to my Party</h1>
-<List people={people}/>
-{form.map(name => {
-  <AddToList name={name}/>
-})} */}
--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import formReducer from '../features/formSlice';
-import reservationReducer from '../features/reservationSlice';
-import customerReducer from '../features/customerSlice';
+
 
 export const store = configureStore({
   reducer: {
-    // form: formReducer,
-    reservations: reservationReducer,
-    customer: customerReducer
+
   },
 });
 
