9adbad2c515a3fe39b7f7ef7c4d934b1ef00f4f0
kabradshaw1 <itllbok82@gmail.com>
2023-03-03 14:59:55 -0500

    able to console log data in form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 import {Controller, useForm} from 'react-hook-form';
-import { RootState } from './app/store';
+// import { RootState } from './app/store';
 import { Form, Button, Row, Col} from 'react-bootstrap';
 
-type Inputs = {
-  firstName: string,
-}
-
 function App() {
-  const { register, control, handleSubmit} = useForm({
+  const { control, handleSubmit } = useForm({
     defaultValues: {
-      firstName: ""
-    }
-  })
+      firstName: "",
+    },
+  });
 
-  const formSubmit = (data: SubmitHandler<FieldValues>) => {
+  const formSubmit = (data: any) => {
+    console.log(data);
+  };
 
-  }
   return (
-    <Form>
-      <Row className="mb-3" onSubmit={handleSubmit(formSubmit)}>
+    <Form noValidate onSubmit={handleSubmit(formSubmit)}>
+      <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>First name</Form.Label>
-          <Controller name="firstName" control={control}/>
-          <Form.Control
-            type="text"
-            placeholder="First name"
-          />
+          <Controller name="firstName" control={control} render={({field}) => (<Form.Control type="text" {...field} placeholder="First name"/>)}/>
         </Form.Group>
       </Row>
+      <Button type="submit">Submit Form</Button>
     </Form>
   );
 }
