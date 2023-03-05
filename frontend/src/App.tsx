77e3ea753ea636d18b178e79947bc68f0a45b4f3
kabradshaw1 <itllbok82@gmail.com>
2023-03-05 08:12:39 -0500

    small change to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { RootState } from './app/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './store';
 import UserForm from './components/UserForm'
 
 
 
 function App() {
   return (
-    <UserForm></UserForm>
+    <UserForm/>
   )
 }
 
