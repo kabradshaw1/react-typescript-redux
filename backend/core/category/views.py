86d6ae79461a23faf55184a84ae90d1b8beec0fa
kabradshaw1 <itllbok82@gmail.com>
2023-03-05 16:01:07 -0500

    setting server api

--- backend/core/category/views.py
+++ backend/core/category/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from core.category.serializers import CategorySerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+  serializer_class = CategorySerializer
+
+  def get_queryset(self):
+    return Category.objects.all
--- backend/core/item/models.py
+++ backend/core/item/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.category.models import Category
 
-# Create your models here.
+class Item(models.Model):
+  name = models.CharField(max_length=50)
+  category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+  price = models.PositiveIntegerField(required=True)
+  image = models.CharField(max_length=100)
+  description = models.textField(max_length=250)
+  quantity = models.PositiveIntegerField(default=0)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
