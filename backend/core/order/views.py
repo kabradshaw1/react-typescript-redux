8d6822ec14acac10fdd861948103e0ecc2644e58
kabradshaw1 <itllbok82@gmail.com>
2023-03-06 13:17:05 -0500

    wrote order views

--- backend/core/order/views.py
+++ backend/core/order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.order.models import Order
+from core.order.serializers import OrderSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+  serializer_class = OrderSerializer
+
+  def get_queryset(self):
+    return Order.objects.all()
