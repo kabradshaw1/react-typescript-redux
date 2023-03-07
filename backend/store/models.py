697c9a0746639e995f3c45c23721507f5429495a
kabradshaw1 <itllbok82@gmail.com>
2023-03-07 17:43:34 -0500

    working on server

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+'''
+not really sure I think there is any reason to have a category at this point 
+I could just as easily have this as a property in the items class.  I guess 
+I'll leave it in here for practice
+'''
 class Category(models.Model):
   name = models.CharField(max_length=100)
 
@@ -8,11 +12,21 @@ class Category(models.Model):
     return self.name
 
 class Item(models.Model):
+
+  # options = (
+  #   'Food',
+  #   'Household Supplies',
+  #   'Electronics',
+  #   'Books',
+  #   'Toys',
+  # )
+
+  # category = models.CharField(max_length=20, choices=options)
   category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
   name = models.CharField(max_length=50, unique=True)
-  price = models.DecimalField(required=True)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
   image = models.CharField(max_length=100)
-  description = models.textField(max_length=250)
+  description = models.TextField(max_length=250)
   quantity = models.PositiveIntegerField(default=0)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
@@ -25,3 +39,6 @@ class Order(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.id
--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
-from django.views.generic import templateView
+from django.views.generic import TemplateView
 
+app_name = 'store'
+
+urlpatterns = [
+  path('', TemplateView.as_view(template_name='index.html'))
+]
