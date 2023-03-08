b2b9bef89c60f2c96fe1299a78dfedff21d31ddb
kabradshaw1 <itllbok82@gmail.com>
2023-03-08 17:40:41 -0500

    working on views

--- backend/store_api/views.py
+++ backend/store_api/views.py
@@ -1,5 +1,5 @@
 from rest_framework
-from store.models import Item, Order
+from store.models import Item, Order, Category,
 from .serializers import ItemSerializer, OrderSerializer, CategorySerializer
 
 
