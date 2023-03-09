e6252bfb5a897412ab981e68bed6821eadfcafb4
kabradshaw1 <itllbok82@gmail.com>
2023-03-09 12:13:01 -0500

    learning about admin page and admin.py file

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . import models
 
-@admin.register(models.Item)
-class 
+@admin.register(models.OrderedItems)
+class ItemAdmin(admin.ModelAdmin):
+  list_display = ('')
+
+admin.site.register(models.Category)
