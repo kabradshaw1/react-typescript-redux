898a3d259be2104a15335eef44464a24b27def6f
kabradshaw1 <itllbok82@gmail.com>
2023-03-09 17:36:08 -0500

    roughtly done with writing tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AppowAny',
+        'rest_framework.permissions.AllowAny',
     ]
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
 #         'rest_framework_simplejwt.authentication.JWTAuthentication',
--- backend/store/tests.py
+++ backend/store/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Item, Category, Order, OrderedItems
 
-# Create your tests here.
+class Test_Create_Item(TestCase):
+
+  @classmethod
+  def setUpTestData(cls):
+    test_category = Category.objects.create(name='Food')
+    test_user = User.objects.create_user(username='test_user1', password='12345678')
+    # in the Very Academy tutorial, he used the category_id.  I'm not currently
+    # sure where he got that.
+    test_item = Item.objects.create(category_id=1, name='stuff', price=2.00, image='test.jpg', description='test test test', quantity=200)
+    test_order = Order.objects.create(item_id=1, username='test_user1')
+    test_odrdered_items = OrderedItems.objects.create(item_id=1, order_id=1, quantity=1)
+  
+  def test_store_content(self):
+    item = Item.objects.get(id=1) 
+    order = Order.objects.get(id=1)
+    category = Category.objects/get(id=1)
+    item_name = f'{item.name}'
+    item_price = f'{item.price}'
+    item_image = f'{item.image}'
+    item_description = f'{item.description}'
+    item_quantity = f'{item.quantity}'
+    order_username = f'{order.username}'
+    self.assertEqual(item_name, 'stuff')
+    self.assertEqual(item_price, 2.00)
+    self.assertEqual(item_image, 'test.jpg')
+    self.assertEqual(item_description, 'test test test')
+    self.assertEqual(item_quantity, 200)
+    self.assertEqual(order_username, 'test_user1')
+    self.assertEqual(str(item), "stuff")
+    self.assertEqual(str(order), 1)
+    self.assertEqual(str(category), 'Food')
+    
