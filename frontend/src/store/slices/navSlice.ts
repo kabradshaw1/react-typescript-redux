11a7c982ac3c46f82fa9367575a14f872840e90f
kabradshaw1 <itllbok82@gmail.com>
2023-03-10 10:46:52 -0500

    think this is the right slice for nav

--- frontend/src/store/slices/navSlice.ts
+++ frontend/src/store/slices/navSlice.ts
@@ -12,8 +12,11 @@ export const navSlice = createSlice({
   name: "nav",
   initialState,
   reducers: {
-    updateNav: (state, action: PayloadAction<NavState>) => {
-      state.value.push(action.payload);
+    updateNav: (state, action: PayloadAction<string>) => {
+      state.value = action.payload
     }
   }
 })
+
+export const {updateNav} = navSlice.actions
+export default navSlice.reducer
