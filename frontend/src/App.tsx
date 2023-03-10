59696ff3bbce75b8953139ae39dc19dec9c04465
kabradshaw1 <itllbok82@gmail.com>
2023-03-09 21:44:00 -0500

    working on fontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import './App.css';
 import { useSelector } from 'react-redux';
 import { RootState } from './store';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Router } from 'react-router-dom'
 import UserForm from './components/UserForm'
 import axios from 'axios';
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('root')!;
@@ -20,7 +20,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
