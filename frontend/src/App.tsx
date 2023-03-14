002d196497fbbce9d7648fa6292f399ace22762f
kabradshaw1 <itllbok82@gmail.com>
2023-03-14 15:19:57 -0400

    front end broken and working through it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import './App.css';
 import { useSelector } from 'react-redux';
@@ -6,14 +6,42 @@ import { RootState } from './store';
 import { Routes, Route, Router } from 'react-router-dom'
 import UserForm from './components/UserForm'
 import axios from 'axios';
+import AllItems from './components/AllItems';
+import ItemsLoadingComponent from './components/ItemsLoadingComponent';
 
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
+interface Items {
+  price: number,
+  name: string,
+  description: string,
+  category: string,
+  quantity: number,
+  image: string,
+  created: Date,
+  updated: Date,
+}
+
+interface AppState {
+  [
+    loading: boolean,
+    items: Items | null
+  ]
+}
 
 function App() {
 
   const nav = useSelector((state: RootState) => state.nav.value);
+  const ItemsLoading = ItemsLoadingComponent(AllItems)
+  const [appState, setAppState] = useState<AppState>([
+    loading: false,
+    items: null
+  ]);
+  useEffect(() => {
+    setAppState({ loading: true });
+    axios.get('http://127.0.0.1:8000/api/item')
+  })
 
   return (
     <>
---
+++ frontend/src/components/ItemsLoadingComponent/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ItemsLoadingComponent(Component) {
+  return (
+    <div>ItemsL</div>
+  )
+}
