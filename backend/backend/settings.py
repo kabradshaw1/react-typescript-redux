f9781b02503dd9b7b6eac4ce3b140a2713a2d622
kabradshaw1 <itllbok82@gmail.com>
2023-05-23 09:00:37 -0400

    Debugging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,10 +97,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'NAME': os.environ.get('DB_NAME', 'store'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS', 'TDYoung17!')
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
 
