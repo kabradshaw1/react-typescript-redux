daca8a33b40e6da12cf34e9b55c00387c97559f1
kabradshaw1 <itllbok82@gmail.com>
2023-05-31 16:51:28 -0400

    working on building containers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,10 +100,9 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS')
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
