13b95fe481be864a612994e6975b82f4e1a19485
kabradshaw1 <itllbok82@gmail.com>
2023-06-01 10:08:42 -0400

    all end points are now working on container

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,13 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
-CORS_ALLOWED_ORIGINS = []
-CORS_ALLOWED_ORIGINS.extend(
-    filter(
-        None,
-        os.environ.get('CORS_ALLOWED_ORIGINS', '').split(','),
-    )
-)
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1']
+
 # CORS_ALLOW_ALL_ORIGINS: True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
