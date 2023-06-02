d9267d8a523bb3513248bbfc20bdd561c0116d91
kabradshaw1 <itllbok82@gmail.com>
2023-06-02 09:41:21 -0400

    updated cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://localhost:3000', 'ec2-52-91-48-17.compute-1.amazonaws.com']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://localhost:3000', 'http://ec2-52-91-48-17.compute-1.amazonaws.com']
 
 # CORS_ALLOW_ALL_ORIGINS: True
 
