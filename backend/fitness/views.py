f98cfea95d97e37d9603362698e5e79d9b7c5be6
kabradshaw1 <itllbok82@gmail.com>
2023-06-07 05:06:35 -0400

    working on views for fitness

--- backend/fitness/views.py
+++ backend/fitness/views.py
@@ -2,4 +2,16 @@ from user.models import User
 from .models import Active, Heart, Steps, Weight, Demo
 from .serializers import ActiveSerializer, HeartSerializer, StepsSerializer, WeightSerializer, DemoSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
+
+class ActiveViewSet(viewsets.ModelViewSet):
+  serializer_class = ActiveSerializer
+  permission_classes = [IsAuthenticated]
+
+  def get_queryset(self):
+    user_id = self.request.user.id
+    queryset = Active.objects.filter(user=user_id)
+    return queryset
+
+  
