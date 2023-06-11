368ba1feff0d3b442cc02d4d6cd03221a7200292
kabradshaw1 <itllbok82@gmail.com>
2023-06-11 08:57:00 -0400

    working on frontend

--- frontend/src/portfolio/Header/index.tsx
+++ frontend/src/portfolio/Header/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Container from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const PortfolioHeader: React.FC = () => {
   return (
-    <Navbar as='header' bg='secondary' expand="lg" className='sticky-top'>
+    <Navbar as='header' bg='primary' expand="lg" className='sticky-top'>
+      <Container>
         <LinkContainer to=''>
           <Navbar.Brand>Kyle's Bio & Project Links</Navbar.Brand>
         </LinkContainer>
+      </Container>
     </Navbar>
   )
 };
