f3328da412d086ec1c1c18ffc417a42fce333645
kabradshaw1 <itllbok82@gmail.com>
2023-06-14 17:55:10 -0400

    working on logged in fitness page

--- frontend/src/fitness/components/LoggedIn/index.tsx
+++ frontend/src/fitness/components/LoggedIn/index.tsx
@@ -4,14 +4,22 @@ import {Data} from '../../../utils/types';
 import { fetcher } from '../../../utils/axios';
 import useSWR from 'swr';
 import Container from 'react-bootstrap/Container';
-
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store';
 
 const LoggedIn: React.FC = () => {
+
+  const currentFitness = useSelector((state: RootState) => state.fitness.value);
+  console.log(currentFitness)
   const {data: qty, error, isLoading } = useSWR<Data[]>(`/fitness/`, fetcher);
+
   if(error) return <Container><h3>Failed to load.</h3></Container>
   if(isLoading) return <Container><h3>Loading...</h3></Container>
   return (
-    <h2>logged in</h2>
+    <Container>
+      {/* <Table data={qty}/> */}
+      <InputBox endpointProp={currentFitness}/>
+    </Container>
   )
 }
 
