9fc5ee28c9a78cbdb5612d507c0c2492c3f3eeda
kabradshaw1 <itllbok82@gmail.com>
2023-06-15 10:59:19 -0400

    working on header

--- frontend/src/universal/components/Header/subheaders/FitnessSubHeader/index.tsx
+++ frontend/src/universal/components/Header/subheaders/FitnessSubHeader/index.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
+import { RootState } from '../../../../../store';
+import Nav from 'react-bootstrap/Nav';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const FitnessHeader: React.FC = () => {
+
+  const dispatch = useDispatch();
+
+  const fitness = useSelector((state:RootState) => state.fitness.value);
+  const isLoggedIn = useSelector((state: RootState) => state.auth.account);
+
   return (
     <Navbar as='header' bg='light' expand="lg" className='sticky-top subheader'>
       <Container>
-        <LinkContainer to=''>
-          <Navbar.Brand>Fitness</Navbar.Brand>
-        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <LinkContainer to=''>
+            <Navbar.Brand>Fitness</Navbar.Brand>
+          </LinkContainer>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
