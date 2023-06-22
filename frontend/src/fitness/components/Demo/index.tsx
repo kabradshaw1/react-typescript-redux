66b2de35614dc5b8ce3b039b01578bab13066cf6
kabradshaw1 <itllbok82@gmail.com>
2023-06-21 22:07:39 -0400

    update to nginx

--- frontend/src/fitness/components/Demo/index.tsx
+++ frontend/src/fitness/components/Demo/index.tsx
@@ -16,42 +16,42 @@ interface Demo {
 
 const Demo: React.FC = () => {
   const {data: demo, error, isLoading } = useSWR<Demo[] | undefined>('/fitness/demo/', fetcher);
-
+  console.log(demo)
   if(error) return <h3>Failed to load.</h3>
   if(isLoading) return <h3>Loading...</h3>
   if(!demo) return null;
 
-  const activeData: Data[] = demo && demo.map(item => ({
-    created: item.created,
-    data: item.active,
-    id: item.id
-  }));
-  const heartData: Data[] = demo && demo.map(item => ({
-    created: item.created,
-    data: item.heart,
-    id: item.id
-  }));
-  const stepsData: Data[] = demo && demo.map(item => ({
-    created: item.created,
-    data: item.steps,
-    id: item.id
-  }));
-  const weightData: Data[] = demo && demo.map(item => ({
-    created: item.created,
-    data: item.weight,
-    id: item.id
-  }));
+  // const activeData: Data[] = demo && demo.map(item => ({
+  //   created: item.created,
+  //   data: item.active,
+  //   id: item.id
+  // }));
+  // const heartData: Data[] = demo && demo.map(item => ({
+  //   created: item.created,
+  //   data: item.heart,
+  //   id: item.id
+  // }));
+  // const stepsData: Data[] = demo && demo.map(item => ({
+  //   created: item.created,
+  //   data: item.steps,
+  //   id: item.id
+  // }));
+  // const weightData: Data[] = demo && demo.map(item => ({
+  //   created: item.created,
+  //   data: item.weight,
+  //   id: item.id
+  // }));
 
   return (
   <Row>
-    <h2>Active Energy</h2>
+    {/* <h2>Active Energy</h2>
     <Table data={activeData}/>
     <h2>Weight</h2>
     <Table data={weightData}/>
     <h2>Steps</h2>
     <Table data={stepsData}/>
     <h2> Max Heart Rate</h2>
-    <Table data={heartData}/>
+    <Table data={heartData}/> */}
   </Row>
   )
 };
