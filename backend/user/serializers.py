8ca23848bf2472c189fda833c7d374c628cd3e24
kabradshaw1 <itllbok82@gmail.com>
2023-06-23 09:11:58 -0400

    adding comments and cleaning up backend

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -13,12 +13,14 @@ from rest_framework_simplejwt.settings import api_settings
 from user.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'is_active', 'created', 'updated']
         read_only_field = ['is_active', 'created', 'updated']
 
 class RegisterSerializer(UserSerializer):
+    """Serializer for register users."""
     password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
     email = serializers.EmailField(required=True, write_only=True, max_length=128)
 
@@ -27,6 +29,7 @@ class RegisterSerializer(UserSerializer):
         fields = ['id', 'username', 'email', 'password', 'is_active', 'created', 'updated']
 
     def create(self, validated_data):
+        """Create and return a user with encrypted password."""
         try:
             user = User.objects.get(email=validated_data['email'])
         except ObjectDoesNotExist:
@@ -34,6 +37,7 @@ class RegisterSerializer(UserSerializer):
         return user
 
 class LoginSerializer(TokenObtainPairSerializer):
+    """Serializer for the user auth token."""
 
     def validate(self, attrs):
         """Validate and authenticate the user."""
--- backend/user/views.py
+++ backend/user/views.py
@@ -3,7 +3,7 @@ Views for user APIs.
 """
 
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework import filters, viewsets, status
 from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import AllowAny
@@ -16,17 +16,29 @@ from user.models import User
 from user.serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
+    """Retrieves a user that's in the system."""
     http_method_names = ['get']
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticated]
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['updated']
     ordering = ['-updated']
 
+    def get_permissions(self):
+        """Returns the permission classes based on the request method."""
+        if self.action == 'list':  # Applies to the `get_queryset` method
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = self.permission_classes
+
+        return [permission() for permission in permission_classes]
+
     def get_queryset(self):
+        """Retrieves all users."""
         return User.objects.all()
 
     def get_object(self):
+        """Retrieves a single user."""
         lookup_field_value = self.kwargs[self.lookup_field]
 
         obj = User.objects.get(id=lookup_field_value)
@@ -35,11 +47,13 @@ class UserViewSet(viewsets.ModelViewSet):
         return obj
 
 class LoginViewSet(ViewSet):
+    """Create a new auth token for user"""
     serializer_class = LoginSerializer
     permission_classes = (AllowAny,)
     http_method_names = ['post']
 
     def create(self, request, *args, **kwargs):
+        """Accepts login information, varifies it's currect, then returns user, refresh, and access tokens"""
         serializer = self.serializer_class(data=request.data)
 
         try:
@@ -50,6 +64,7 @@ class LoginViewSet(ViewSet):
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
+    """Create a new refresh token."""
     permission_classes = (AllowAny,)
     http_method_names = ['post']
 
@@ -64,6 +79,7 @@ class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 class RegistrationViewSet(ViewSet):
+    """Create a new user in the system."""
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
     http_method_names = ['post']
